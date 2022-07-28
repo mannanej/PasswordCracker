d584c15c28865d00ea6045efbb4598353950bf1c
mannanej <mannanej@rose-hulman.edu>
2022-07-28 01:12:41 -0400

    Added a multithread feature

--- PasswordCracker/PasswordCracker.cpp
+++ PasswordCracker/PasswordCracker.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <thread>
+#include <vector>
 #include <chrono>
 
 using namespace std;
 using namespace std::chrono;
+int state = 0;
+string multiThreadAnswer = "";
 bool boolNumbers = false;
 bool boolLowerCase = false;
 bool boolUpperCase = false;
@@ -126,16 +130,162 @@ void specific() {
 
     cout << "" << "\n";
     cout << "Your Password Must Be: " << answer << " After A Total Of: " << tries << " Checks!\n";
-    cout << "This Process Took ~" << duration.count() << "ms!";
-    cout << "" << "\n";
+    cout << "This Process Took ~" << duration.count() << "ms!\n" << "\n";
 }
 
 void bruteForce() {
 
-    boolNumbers = true;
-    boolLowerCase = true;
-    boolUpperCase = true;
-    boolSpecial = true;
+    std::string password, answer;
+    long long int tries = 0;
+
+    cout << "What is your password: \n";
+    cin >> password;
+
+    auto start = high_resolution_clock::now();
+
+    for (int i = 0; i < password.size(); i++) {
+            for (int j = 0; j < 10; j++) {
+                if (password.at(i) == numbers[j]) {
+                    answer.push_back(numbers[j]);
+                    cout << "Checking Character: " << numbers[j] << " At Position: " << i << "\n";
+                    tries++;
+                    break;
+                }
+                else {
+                    cout << "Checking Character: " << numbers[j] << " At Position: " << i << "\n";
+                    tries++;
+                }
+            }
+        
+            for (int k = 0; k < 26; k++) {
+                if (password.at(i) == lowerCase[k]) {
+                    answer.push_back(lowerCase[k]);
+                    cout << "Checking Character: " << lowerCase[k] << " At Position: " << i << "\n";
+                    tries++;
+                    break;
+                }
+                else {
+                    cout << "Checking Character: " << lowerCase[k] << " At Position: " << i << "\n";
+                    tries++;
+                }
+            }
+        
+            for (int l = 0; l < 26; l++) {
+                if (password.at(i) == upperCase[l]) {
+                    answer.push_back(upperCase[l]);
+                    cout << "Checking Character: " << upperCase[l] << " At Position: " << i << "\n";
+                    tries++;
+                    break;
+                }
+                else {
+                    cout << "Checking Character: " << upperCase[l] << " At Position: " << i << "\n";
+                    tries++;
+                }
+            }
+        
+            for (int p = 0; p < 30; p++) {
+                if (password.at(i) == special[p]) {
+                    answer.push_back(special[p]);
+                    cout << "Checking Character: " << special[p] << " At Position: " << i << "\n";
+                    tries++;
+                    break;
+                }
+                else {
+                    cout << "Checking Character: " << special[p] << " At Position: " << i << "\n";
+                    tries++;
+                }
+            }
+    }
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    cout << "" << "\n";
+    cout << "Your Password Must Be: " << answer << " After A Total Of: " << tries << " Checks!\n";
+    cout << "This Process Took ~" << duration.count() << "ms!\n" << "\n";
+}
+
+void testChar(char c, int threadNumber) {
+
+    char nextChar{};
+    bool found = false;
+
+    for (int j = 0; j < 10; j++) {
+        if (c == numbers[j]) {
+            found = true;
+            nextChar = numbers[j];
+            break;
+        }
+    }
+
+    if (found == false) {
+        for (int k = 0; k < 26; k++) {
+            if (c == lowerCase[k]) {
+                found = true;
+                nextChar = lowerCase[k];
+                break;
+            }
+        }
+    }
+
+    if (found == false) {
+        for (int l = 0; l < 26; l++) {
+            if (c == upperCase[l]) {
+                found == true;
+                nextChar = upperCase[l];
+                break;
+            }
+        }
+    }
+
+    if (found == false) {
+        for (int p = 0; p < 30; p++) {
+            if (c == special[p]) {
+                found == true;
+                nextChar = special[p];
+                break;
+            }
+        }
+    }
+    
+    while (threadNumber != state) {
+        // Wait
+    }
+    multiThreadAnswer.push_back(nextChar);
+    state++;
+}
+
+void multiThread() {
+
+    std::string password, answer;
+    long long int tries = 0;
+
+    cout << "What is your password: \n";
+    cin >> password;
+
+    auto start = high_resolution_clock::now();
+
+    std::vector<std::thread> threads(password.length());
+
+    for (int i = 0; i < password.length(); i++) {
+        std::thread th(testChar, password.at(i), i);
+        threads.push_back(std::move(th));
+    }
+
+    for (std::thread & th : threads) {
+        if (th.joinable()) {
+            th.join();
+        }
+    }
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    cout << "" << "\n";
+    cout << "Your Password Must Be: " << multiThreadAnswer << "\n";
+    cout << "This Process Took ~" << duration.count() << "ms!\n" << "\n";
+    multiThreadAnswer = "";
+    state = 0;
 }
 
 int main() {
@@ -144,7 +294,7 @@ int main() {
         std::string password;
         int process;
         cout << "Welcome to the Password Cracker!\nHow would you like us to guess your password?\n";
-        cout << "1: Get Specific\n2: Brute Force\n";
+        cout << "1: Get Specific\n2: Brute Force\n3: Multithread Brute Force\n";
         cout << "Choice: ";
         cin >> process;
 
@@ -159,10 +309,10 @@ int main() {
             specific();
         }
         else if (process == 2) {
-            cout << " " << "\n";
-            printf("What is your password: \n");
-            cin >> password;
             bruteForce();
+        }
+        else if (process == 3) {
+            multiThread();
         }
         else {
             cout << " " << "\n";
